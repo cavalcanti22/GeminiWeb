2da832c938bb6c61a1ada0b9ffaa2135755daf54
Rodrigo Cavalcanti <cavalcanti22@gmail.com>
2018-11-20 14:36:45 -0200

    OneSignal adicionado ao layout

--- views/layouts/site.php
+++ views/layouts/site.php
@@ -32,104 +32,114 @@
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-      <![endif]-->
-    </head>
-    <body>
+    <![endif]-->
 
-     <!--===== HEADER SECTION =====-->
-     <div class="preloader">
-      <div class="spinner">
-      </div><!--spinner-->
-    </div><!--end-preloader-->
-    <a class="back-to-top scrollTo back-to-top--active" href="index.html#site-header">
-      <span class="fa fa-angle-up"></span>
-    </a><!--back-to-top-->
-    <div id="home"></div>
-    <header id="site-header" class="home-header">
-      <div id="particles-js"></div>
-      <nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-12 col-sm-12 col-xs-12">
-              <!-- Brand and toggle get grouped for better mobile display -->
-              <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                  <span class="sr-only">Toggle navigation</span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#home">Gemini <i class="fa fa-laptop" aria-hidden="true"></i></a>
-              </div>
-              <!-- Collect the nav links, forms, and other content for toggling -->
-              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                  <li><a class="active" href="#home">Home</a>
-                  </li>
-                  <li><a href="#about">Sobre Nós</a>
-                  </li>
-                  <li><a href="#service">Serviços</a>
-                  </li>
+    <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+    <script>
+      var OneSignal = window.OneSignal || [];
+      OneSignal.push(function() {
+        OneSignal.init({
+          appId: "585e2142-29b4-4e8c-8e37-adda7fa04907",
+        });
+      });
+    </script>
+  </head>
+  <body>
+
+   <!--===== HEADER SECTION =====-->
+   <div class="preloader">
+    <div class="spinner">
+    </div><!--spinner-->
+  </div><!--end-preloader-->
+  <a class="back-to-top scrollTo back-to-top--active" href="index.html#site-header">
+    <span class="fa fa-angle-up"></span>
+  </a><!--back-to-top-->
+  <div id="home"></div>
+  <header id="site-header" class="home-header">
+    <div id="particles-js"></div>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-sm-12 col-xs-12">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#home">Gemini <i class="fa fa-laptop" aria-hidden="true"></i></a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+                <li><a class="active" href="#home">Home</a>
+                </li>
+                <li><a href="#about">Sobre Nós</a>
+                </li>
+                <li><a href="#service">Serviços</a>
+                </li>
                             <!-- <li><a href="index.html#portfolio">portfolio</a>
-                          </li> -->
+                            </li> -->
                             <!-- <li><a href="index.html#testimonials">testimonials</a>
-                          </li> -->
-                          <li><a href="#contact">Contato</a>
-                          </li>
-                        </ul>
+                            </li> -->
+                            <li><a href="#contact">Contato</a>
+                            </li>
+                          </ul>
+                        </div>
+                        <!-- /.navbar-collapse -->
                       </div>
-                      <!-- /.navbar-collapse -->
+                      <!-- /.col-md-12 -->
                     </div>
-                    <!-- /.col-md-12 -->
+                    <!-- /.row -->
                   </div>
-                  <!-- /.row -->
+                  <!-- /.container -->
+                </nav>
+                <!--nav-->
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-12 col-sm-12 header-text">
+                      <h1><?=$abertura['titulo']?></h1>
+                      <p><?=$abertura['subtitulo']?> <span id="typer"></span></p>
+                      <a href="#contact"><button class="btn-default"><?=$contato['telefone']?></button></a>
+                    </div>
+                    <!--col-md-12-->
+                  </div>
+                  <!--row-->
                 </div>
-                <!-- /.container -->
-              </nav>
-              <!--nav-->
-              <div class="container">
+                <!--container-->
+              </header>
+              <!--end-header-section-->
+
+              <?php require $view.".php";?>
+              <!--===== FOOTER SECTION =====-->
+              <footer class="footer-section">
+               <div class="container">
                 <div class="row">
-                  <div class="col-md-12 col-sm-12 header-text">
-                    <h1><?=$abertura['titulo']?></h1>
-                    <p><?=$abertura['subtitulo']?> <span id="typer"></span></p>
-                    <a href="#contact"><button class="btn-default"><?=$contato['telefone']?></button></a>
-                  </div>
-                  <!--col-md-12-->
+                 <div class="col-md-12 col-sm-12 footer-text wow fadeIn" data-wow-delay="0.7s">
+                  <p>Copyright © 2017 By <b>Gemini Web Floripa</b> | Todos os Direitos Reservados</p>
+                  <p><i class="fa fa-whatsapp" aria-hidden="true"></i> <?=$contato['telefone']?></p>
+                  <p><i class="fa fa-envelope-o" aria-hidden="true"></i> <?=$contato['email']?></p>
                 </div>
-                <!--row-->
+                <!--col-md-12-->
               </div>
-              <!--container-->
-            </header>
-            <!--end-header-section-->
+              <!--row-->
+            </div>
+            <!--container-->
+          </footer>
+          <!--end-footer-section-->
 
-     <?php require $view.".php";?>
-<!--===== FOOTER SECTION =====-->
-<footer class="footer-section">
- <div class="container">
-  <div class="row">
-   <div class="col-md-12 col-sm-12 footer-text wow fadeIn" data-wow-delay="0.7s">
-    <p>Copyright © 2017 By <b>Gemini Web Floripa</b> | Todos os Direitos Reservados</p>
-    <p><i class="fa fa-whatsapp" aria-hidden="true"></i> <?=$contato['telefone']?></p>
-    <p><i class="fa fa-envelope-o" aria-hidden="true"></i> <?=$contato['email']?></p>
-  </div>
-  <!--col-md-12-->
-</div>
-<!--row-->
-</div>
-<!--container-->
-</footer>
-<!--end-footer-section-->
-
-<!--===== JAVASCRIPT FILES =====-->  
-<script type="text/javascript" src="<?=site_url()?>resources/site/js/jquery-2.1.4.min.js"></script>
-<script type="text/javascript" src="<?=site_url()?>resources/site/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="<?=site_url()?>resources/site/js/jquery.magnific-popup.js"></script>
-<script type="text/javascript" src="<?=site_url()?>resources/site/js/jquery-mixitup.js"></script>
-<script type="text/javascript" src="<?=site_url()?>resources/site/js/wow.js"></script>
-<script type="text/javascript" src="<?=site_url()?>resources/site/js/particles.min.js"></script>
-<script type="text/javascript" src="<?=site_url()?>resources/site/js/app.js"></script>
-<script type="text/javascript" src="<?=site_url()?>resources/site/js/jquery.typer.js"></script>
-<script type="text/javascript" src="<?=site_url()?>resources/site/js/jquery-validator.js"></script>
-<script type="text/javascript" src="<?=site_url()?>resources/site/js/main.js"></script>
-</body>
-</html>
+          <!--===== JAVASCRIPT FILES =====-->  
+          <script type="text/javascript" src="<?=site_url()?>resources/site/js/jquery-2.1.4.min.js"></script>
+          <script type="text/javascript" src="<?=site_url()?>resources/site/js/bootstrap.min.js"></script>
+          <script type="text/javascript" src="<?=site_url()?>resources/site/js/jquery.magnific-popup.js"></script>
+          <script type="text/javascript" src="<?=site_url()?>resources/site/js/jquery-mixitup.js"></script>
+          <script type="text/javascript" src="<?=site_url()?>resources/site/js/wow.js"></script>
+          <script type="text/javascript" src="<?=site_url()?>resources/site/js/particles.min.js"></script>
+          <script type="text/javascript" src="<?=site_url()?>resources/site/js/app.js"></script>
+          <script type="text/javascript" src="<?=site_url()?>resources/site/js/jquery.typer.js"></script>
+          <script type="text/javascript" src="<?=site_url()?>resources/site/js/jquery-validator.js"></script>
+          <script type="text/javascript" src="<?=site_url()?>resources/site/js/main.js"></script>
+        </body>
+        </html>
